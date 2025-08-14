d54ab582339aaee036e60cba78e37365a7643bac
Khushi Gosavi <gosavikhushi62@gmail.com>
2025-08-14 14:13:38 +0530

    Add files via upload

---
+++ bankacc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankacc
+{
+    class Bankacc
+    {
+        public double Balance;
+
+        public void Deposit(double amt)
+        {
+            Balance += amt;
+            Console.WriteLine("Deposited" + amt);
+        }
+        public void withdraw(double amt)
+        {
+            if (amt <= Balance)
+            {
+                Balance -= amt;
+
+                Console.WriteLine("withdrawan:" + amt);
+            }
+            else
+                Console.WriteLine("Not enough balance");
+        }
+    }
+    class SavingAcc : Bankacc
+    {
+        public double Intrestrate;
+
+        public void CalculateIntrest()
+
+        {
+            Console.WriteLine("Intrest:" + (Balance * Intrestrate / 100));
+        }
+
+    }
+}
+
+    
+
