589902c1a9be604041dae8c8ad845c72e918ae3a
Khushi Gosavi <gosavikhushi62@gmail.com>
2025-09-04 14:18:44 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
-﻿namespace ReverseOfNumber
+﻿namespace Sum
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    
+        public class Program
         {
-            Console.WriteLine("Enter a number to find the reverse:");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Reverse r = new Reverse(num);
-            r.process();
-            r.display();
+            public static void Main(string[] args)
+            {
+                static void sum()
+                {
+                    sum obj = new sum();
+                    obj.ReadNumber();
+                    obj.calculatesum();
+                    obj.display();
+
+                }
+                sum();
+            }
         }
     }
-}
