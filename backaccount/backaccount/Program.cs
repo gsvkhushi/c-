1d9729b916db48d2d39f79b2c8dfe1002614c1f8
Khushi Gosavi <gosavikhushi62@gmail.com>
2025-09-04 15:01:15 +0530

    Add files via upload

---
+++ backaccount/backaccount/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace backaccount
+{
+   
+class Program
+        {
+            static void Main(string[] args)
+            {
+                SavingAcc mySavings = new SavingAcc();
+                mySavings.Balance = 1000;
+                mySavings.Intrestrate = 5;
+                mySavings.Deposit(500);
+                mySavings.withdraw(200);
+                mySavings.CalculateIntrest();
+                Console.WriteLine("Final Balance: " + mySavings.Balance);
+                Console.ReadLine();
+            }
+        }
+
+    }
+
---
+++ backaccount/backaccount/bankacc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backaccount
+{
+       class Bankacc
+        {
+            public double Balance;
+
+            public void Deposit(double amt)
+            {
+                Balance += amt;
+                Console.WriteLine("Deposited" + amt);
+            }
+            public void withdraw(double amt)
+            {
+                if (amt <= Balance)
+                {
+                    Balance -= amt;
+
+                    Console.WriteLine("withdrawan:" + amt);
+                }
+                else
+                    Console.WriteLine("Not enough balance");
+            }
+        }
+        class SavingAcc : Bankacc
+        {
+            public double Intrestrate;
+
+            public void CalculateIntrest()
+
+            {
+                Console.WriteLine("Intrest:" + (Balance * Intrestrate / 100));
+            }
+
+        }
+    }
+
+
+
