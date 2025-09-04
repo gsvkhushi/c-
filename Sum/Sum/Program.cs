21fa1a501d70de7f85a721e3d306276a82768015
Khushi Gosavi <gosavikhushi62@gmail.com>
2025-09-04 14:52:05 +0530

    Add files via upload

---
+++ Sum/Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sum
+{
+    
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                static void sum()
+                {
+                    sum obj = new sum();
+                    obj.ReadNumber();
+                    obj.calculatesum();
+                    obj.display();
+
+                }
+                sum();
+            }
+        }
+    }
