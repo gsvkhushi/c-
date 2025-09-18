ee6c2046cc7f630b9d3816f14a91ad9e4553c11b
Khushi Gosavi <gosavikhushi62@gmail.com>
2025-09-18 13:56:27 +0530

    Add files via upload

---
+++ reversenumber/reversenumber/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace reversenumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to find the reverse:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            reverse r = new reverse(num);
+            r.process();
+            r.display();
+        }
+    }
+}
---
+++ reversenumber/reversenumber/reverse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reversenumber
+{
+    internal class reverse
+    {
+        int num;
+        int rev = 0;
+        int temp;
+        public reverse(int num)
+        {
+            this.num = num;
+        }
+        public void process()
+        {
+            while (num > 0)
+            {
+                temp = num % 10;
+                num /= 10;
+                rev = rev * 10 + temp;
+            }
+
+        }
+        public void display()
+        {
+            Console.WriteLine($"The Reversed Number is {rev}");
+        }
+    }
+}
