d53718899aa7ba20ae2c7552c27d775c73e7ee3f
Khushi Gosavi <gosavikhushi62@gmail.com>
2025-09-18 14:05:47 +0530

    Add files via upload

---
+++ librarymanagement/librarymanagement/bookk.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book("Diary of A Wimpy Kid", "Jeff Kinney");
+            b.DisplayDetails();
+            Book e = new EBook("Atomic Habits", "James Clear", 100, "ATH");
+            e.DisplayDetails();
+        }
+    }
+}
