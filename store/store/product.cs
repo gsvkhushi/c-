f416b4802f8ba2d6486dc1cdd8661338d7737f4b
Khushi Gosavi <gosavikhushi62@gmail.com>
2025-09-18 14:19:33 +0530

    Add files via upload

---
+++ store/store/product.cs
@@ -0,0 +1,15 @@
+﻿namespace Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product("Book", 50.50);
+            p.Display();
+            EProduct e = new EProduct("Watch", 2500.99, "Titan", 2);
+            e.Display();
+            Smartphone s = new Smartphone("Smartphone", 48000, "oneplus 11R", 1, "android");
+            s.Display();
+        }
+    }
+}
